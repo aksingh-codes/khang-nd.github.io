dfc0fa45d022de6cb50acf115afb0739d07d14e8
khang-nd <khang.nguyenduyvn@gmail.com>
2020-08-22 22:53:38 +0700

    Updates

---
+++ precache-manifest.e002c6423bc02995ec8954f761856585.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f731060566cc6ea258e2491fda716cbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a311148ae20167320f6",
+    "url": "/static/css/main.17fb63d8.chunk.css"
+  },
+  {
+    "revision": "f3261d09fec0f733b30c",
+    "url": "/static/js/2.8b556ee7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8b556ee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a311148ae20167320f6",
+    "url": "/static/js/main.c34e0047.chunk.js"
+  },
+  {
+    "revision": "1dca4ba425467718080d",
+    "url": "/static/js/runtime-main.b67a49b9.js"
+  },
+  {
+    "revision": "9c6a311406f71db600b5818d4f680331",
+    "url": "/static/media/classic-about.9c6a3114.svg"
+  },
+  {
+    "revision": "141d3b2282a41315173cc7215a722782",
+    "url": "/static/media/classic-edu.141d3b22.svg"
+  },
+  {
+    "revision": "7e19eab305b630026be0f8f561a01730",
+    "url": "/static/media/classic-settings.7e19eab3.svg"
+  },
+  {
+    "revision": "386e758779d5ea2bbe757f31873b7dd9",
+    "url": "/static/media/classic-skills.386e7587.svg"
+  },
+  {
+    "revision": "14492fe1ce6da28731f550ece1a1fd64",
+    "url": "/static/media/classic-work.14492fe1.svg"
+  },
+  {
+    "revision": "cd3014db9f7a85ab58c79bd568b460da",
+    "url": "/static/media/flat-about.cd3014db.svg"
+  },
+  {
+    "revision": "da19925d1f2fb12069e45384b863975c",
+    "url": "/static/media/flat-edu.da19925d.svg"
+  },
+  {
+    "revision": "788d68a604d7fcdab971078d9fa63ce7",
+    "url": "/static/media/flat-settings.788d68a6.svg"
+  },
+  {
+    "revision": "9eec3a40c70bf62fe4eeb3e9f16100dc",
+    "url": "/static/media/flat-skills.9eec3a40.svg"
+  },
+  {
+    "revision": "eec1ccf5a0453840ef23d93e57259aae",
+    "url": "/static/media/flat-work.eec1ccf5.svg"
+  },
+  {
+    "revision": "96d3ef847be6c76b4eca9638ae4cfcb4",
+    "url": "/static/media/social-fandom-active.96d3ef84.png"
+  },
+  {
+    "revision": "6cdd4b71c0fa953535fb2c4b8786c0d4",
+    "url": "/static/media/social-github.6cdd4b71.png"
+  }
+]);
